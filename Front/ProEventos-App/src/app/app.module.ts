9a661cd32b0575ba9d29f9c0cd2711fa299cf7e0
Ivana Batista dos Santos <bisslee@gmail.com>
2021-04-24 21:44:14 -0300

    atualizando versão do angular

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -1,24 +1,35 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
-
+import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EventosComponent } from './eventos/eventos.component';
-import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+
 import { NavComponent } from './nav/nav.component';
+import { DateTimeFormatPipe } from './helper/DateTimeFormat.pipe';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 import { CollapseModule } from 'ngx-bootstrap/collapse';
+
 import { FormsModule } from '@angular/forms';
+
+import { EventoService } from './services/evento.service';
+
+import { EventosComponent } from './eventos/eventos.component';
+import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     EventosComponent,
     PalestrantesComponent,
-      NavComponent
+      NavComponent,
+      DateTimeFormatPipe
    ],
   imports: [
     BrowserModule,
@@ -26,9 +37,21 @@ import { FormsModule } from '@angular/forms';
     HttpClientModule,
     BrowserAnimationsModule,
     CollapseModule.forRoot(),
-    FormsModule
+    FormsModule,
+    TooltipModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    ModalModule.forRoot(),
+    ToastrModule.forRoot({
+      timeOut: 3000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+      progressBar:true
+    }),
+    NgxSpinnerModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+  providers: [EventoService],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,5 +1,10 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { Evento } from '../models/Evento';
+import { EventoService } from '../services/evento.service';
 
 @Component({
   selector: 'app-eventos',
@@ -7,51 +12,80 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss']
 })
 export class EventosComponent implements OnInit {
+  modalRef!: BsModalRef;
+  public eventos: Evento[] = [];
+  public eventosFiltrados: any = [];
+  public widthImg = 180;
+  public marginImg = 2;
+  public showImg = true;
+  private filtroListado = '';
 
-  public eventos: any=[];
-  public eventosFiltrados: any=[];
-  widthImg:number=180;
-  marginImg:number=2;
-  showImg:boolean=true;
-  private _filterList:string='';
-
-  public get filterList():string{
-    return this._filterList;
+  public get filtroLista(): string  {
+    return this.filtroListado;
   }
 
-  public set filterList(value: string){
-    this._filterList=value;
-    this.eventosFiltrados=this.filterList?this.toFilterEvents(this.filterList):this.getEventos();
+  public set filtroLista(value: string) {
+    this.filtroListado = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.getEventos();
   }
 
- toFilterEvents(filterBy: string): any {
-    filterBy = filterBy.toLocaleLowerCase();
+  public filtrarEventos(filtrarPor: string): Evento[] {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
     return this.eventos.filter(
-      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
-                       evento.local.toLocaleLowerCase().indexOf(filterBy) !== -1
-    )
-  }
-
-  constructor( private http: HttpClient) { }
-
-  ngOnInit(): void {
-    this.getEventos();
-
-  }
-
-  public showImage(){
-    this.showImg=!this.showImg;
-  }
-  public getEventos(): void{
-    this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response=>{
-        this.eventos=response;
-        this.eventosFiltrados= this.eventos;
-      },
-
-      error=>console.log(error)
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+                       evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
     );
+  }
 
+  constructor( private eventoService: EventoService
+             , private modalService: BsModalService
+             , private toastrService: ToastrService
+             , private spinner: NgxSpinnerService) { }
+
+  public ngOnInit(): void {
+    this.getEventos();
+    this.spinner.show();
+
+    setTimeout(() => {
+      /** spinner ends after 5 seconds */
+
+    }, 5000);
+
+  }
+
+  public showImage(): void{
+    this.showImg = ! this.showImg;
+  }
+  public getEventos(): void {
+
+    this.eventoService.getEventos().subscribe({
+      next: (eventos: Evento[]) => {
+        this.eventos = eventos;
+        this.eventosFiltrados = this.eventos;
+      },
+      error: (error: any) =>
+      {
+          console.log(error);
+          this.spinner.hide();
+          this.toastrService.error('Erro ao Carregar os eventos', 'Erro');
+
+      },
+      complete: () =>  this.spinner.hide()
+    });
+
+  }
+
+  openModal(template: TemplateRef<any>): void {
+    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
+  }
+
+  confirm(): void {
+    this.modalRef.hide();
+    this.toastrService.success('Mas olha q deu merda mesmo', 'Avisinho');
+  }
+
+  decline(): void {
+    this.modalRef.hide();
   }
 
 }
